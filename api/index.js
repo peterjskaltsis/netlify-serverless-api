32d5e5a6522407c39ea95aa5dbefc95b8a93dd3a
peterjskaltsis <pjs300300@gmail.com>
2020-09-30 21:02:09 +1000

    initial commit

---
+++ api/index.js
@@ -0,0 +1,17 @@
+/**
+ * Export routes just like Vercel's custom Node API.
+ */
+exports.handler = async (event, context) => {
+  try {
+    const subject = event.queryStringParameters.name || "World";
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: `Hello ${subject}` })
+      // // more keys you can return:
+      // headers: { "headerName": "headerValue", ... },
+      // isBase64Encoded: true,
+    };
+  } catch (err) {
+    return { statusCode: 500, body: err.toString() };
+  }
+}
